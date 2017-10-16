255b4e4c35e6755ede4f17b7d70ba75e9383a5ad
Neo44763 <benedikthuber44@googlemail.com>
2017-10-16 22:54:31 +0200

    more parse handshake later

--- lib/client.ex
+++ lib/client.ex
@@ -21,41 +21,11 @@ defmodule Torrent.Client do
   end
 
   def connect_peer(peer, info_hash) do
-    sha_info_hash = info_hash
-                    |> Bencoder.encode
-                    |> sha_sum
-
-    handshake = sha_info_hash 
-                |> generate_handshake
-
     %{
-      sha_info_hash: sha_info_hash, 
-      handshake: handshake, 
+      info_hash: info_hash, 
       peer: peer
     } |> Torrent.Peer.connect
   end
 
 
-  defp generate_handshake(sha_info_hash) do
-    # The Number 19 in Binary followed by the Protocol String
-    << 19, "BitTorrent protocol" :: binary >> <>
-      # add 8 Zeros and the SHA Hash from the Tracker info, 20 Bytes long
-    << 0, 0, 0, 0, 0, 0, 0, 0, sha_info_hash :: binary, >> <>
-      # some Peer ID, also 20 Bytes long
-    << generate_peer_id :: binary >>
-  end
-
-  defp generate_peer_id do
-    # TODO: generate better Peer_id
-    id = "BE"
-    version = "0044"
-    :random.seed(:erlang.now)
-    number = :random.uniform(1000000000000)
-    number = number |> Integer.to_string |> String.rjust(13, ?0)
-    "-#{id}#{version}#{number}"
-  end
-
-  defp sha_sum(binary) do
-    :crypto.hash(:sha, binary)
-  end
 end
--- lib/peer.ex
+++ lib/peer.ex
@@ -8,7 +8,7 @@ defmodule Torrent.Peer do
 
     try do
       socket = Socket.TCP.connect!(ip, port, packet: :line) 
-      socket |> say_hello(peer_struct[:handshake])
+      socket |> say_hello(peer_struct[:info_hash])
     rescue
       e -> IO.puts(e.message)
         if e.message != "host is unreachable" do
@@ -18,8 +18,14 @@ defmodule Torrent.Peer do
 
   end
 
-  def say_hello(socket, handshake) do
+  def say_hello(socket, info_hash) do
     IO.puts "init handshake: "
+    sha_info_hash = info_hash
+                    |> Bencoder.encode
+                    |> sha_sum
+
+    handshake = sha_info_hash 
+                |> generate_handshake
     socket |> Socket.Stream.send!(handshake)
     socket |> Socket.Stream.recv! |> hear_hello
   end
@@ -32,5 +38,30 @@ defmodule Torrent.Peer do
 
   def start_talking do
   end
+
+  defp sha_sum(binary) do
+    :crypto.hash(:sha, binary)
+  end
+
+  defp generate_handshake(sha_info_hash) do
+    # The Number 19 in Binary followed by the Protocol String
+    << 19, "BitTorrent protocol" :: binary >> <>
+      # add 8 Zeros and the SHA Hash from the Tracker info, 20 Bytes long
+    << 0, 0, 0, 0, 0, 0, 0, 0, sha_info_hash :: binary, >> <>
+      # some Peer ID, also 20 Bytes long
+    << generate_peer_id :: binary >>
+  end
+
+
+  defp generate_peer_id do
+    # TODO: generate better Peer_id
+    id = "BE"
+    version = "0044"
+    :random.seed(:erlang.now)
+    number = :random.uniform(1000000000000)
+    number = number |> Integer.to_string |> String.rjust(13, ?0)
+    "-#{id}#{version}#{number}"
+  end
+
 
 end
