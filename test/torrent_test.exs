ca3fcda173a755d89855f22916d1b1f17b4f5070
huberb <benedikthuber44@googlemail.com>
2017-12-08 19:50:11 +0100

    tests

--- test/torrent_test.exs
+++ test/torrent_test.exs
@@ -2,7 +2,16 @@ defmodule TorrentTest do
   use ExUnit.Case
   doctest Torrent
 
-  test "greets the world" do
-    assert Torrent.hello() == :world
+  test "file size" do
+    info = %{ length: 1277987, piece_length: 16384 }
+    assert Torrent.Filehandler.num_pieces(info) == 79
+    # assert Torrent.hello() == :world
+
+  end
+
+  test "file size 2" do
+    info = %{ length: 1513308160, piece_length: 524288 }
+    Torrent.Filehandler.num_pieces(info) == 2887
+    # assert Torrent.hello() == :world
   end
 end
